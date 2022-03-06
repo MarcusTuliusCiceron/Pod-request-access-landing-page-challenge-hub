f19f7d11119bbc0ea7507e3c7a67b954cdac81e7
Ciceron <100491130+MarcusTuliusCiceron@users.noreply.github.com>
2022-03-06 14:22:11 +0100

    new idea for script and tablet design complete

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+
+function validateEmail(email) 
+{
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
 document.querySelector('.button_real').addEventListener('click', event => {
     console.log('click');
-    document.querySelector('.error').classList.remove('visible');
+    if (validateEmail(document.querySelector('#input-email').value)){
+        console.log('email valid')
+        document.querySelector('.confirmation').classList = 'confirmation visible valid';
+        onsole.log(document.querySelector('.confirmation').classList);
+        document.querySelector('.confirmation').textContent = 'Email sent';
+
+    }
+    else{
+        console.log('email invalid')
+        document.querySelector('.confirmation').classList = 'confirmation visible invalid';
+        onsole.log(document.querySelector('.confirmation').classList);
+        //document.querySelector('.confirmation').textContent = 'Email sent';
+    }
+    document.querySelector('.confirmation').classList.remove('visible');
 })
+document.querySelector('#input-email').addEventListener('change', function(){
+    console.log('it worked')
+    
+})
+
+
